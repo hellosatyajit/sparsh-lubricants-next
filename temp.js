e77c578f589eb2e1ce6e1cb2fa7613751e214737
iamnihal21 <104316581+iamnihal21@users.noreply.github.com>
2025-05-05 20:47:03 +0530

    Delete temp.js

--- temp.js
+++
@@ -1,8 +0,0 @@
-// test-db-url.ts
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const dbUrl = `mysql://ph16334613591:828by9Px~@p3nlmysql11plsk.secureserver.net:3306/ph16334613591_email`;
-
-console.log('MySQL Connection URL:');
-console.log(dbUrl);
